e2398d2b9e8c4ba9f52991c850c0df414ddc88d7
IvanGodinez21 <ivano.godinez@gmail.com>
2020-03-03 22:30:54 -0600

    Se encapsulo la clase Nombres
    Nombre = Solo lectura
    Apellido Paterno = Solo escritura
    Apellido Materno = Escritura y Lectura

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import Nombre from "./nombre.js";
 
 class Main {
     constructor() {
-        this.fecha = new FechaNacimiento(3, 3, 2005)t
-        this.nombre = new Nombre("Ivan", "Godiinez", "Martinez")
+        this.fecha = new FechaNacimiento(3, 3, 2005)
+        this.nombre = new Nombre("Ivan", "Escritura", "Lectura & Escritura")
     }
 
     testFechaNacimiento() {
@@ -22,9 +22,17 @@ class Main {
         console.log(this.fecha.getFormatoExtendido());
         console.log(this.fecha.getEdad());
     } 
-
     testNombre() {
+        //Solo leer el nombre del constructor (Ivan en consola)
         console.log(this.nombre.getNombre());
+
+        //Solo escribir el Apellido Paterno (undefined en consola)
+        this.nombre.setApePat("Godinez");
+        console.log(this.nombre.setApePat())
+
+        //Escribir y leer el Apellido Materno (Martinez en consola)
+        this.nombre.setApeMat("Martinez");
+        console.log(this.nombre.getApeMat());
 
     }
 }
---
+++ nombre.js
@@ -0,0 +1,33 @@
+export default class Nombre {
+    /**
+     * @param {string} nombres
+     * @param {string} apePat
+     * @param {string} apeMat
+     */
+    constructor (nombres, apePat, apeMat) {
+        this._nombres = nombres
+        this._apePat = apePat
+        this._apeMat = apeMat
+    }
+    getNombre() {
+        return `${this._nombres}` 
+    }
+    setApePat(apePat) {
+        this._apePat = apePat;
+    }
+    setApeMat(apeMat) {
+        this._apeMat = apeMat;
+    }
+    getApeMat() {
+        return `${this._apeMat}`
+    }
+    getNombreCompleto() {
+        return `${this._nombres} ${this._apePat} ${this._apeMat}`
+    }
+    getApellidoNombre() {
+        return `${this._apePat} ${this._apeMat} ${this._nombres}`
+    }
+    getIniciales() {
+        return `${this._nombres.charAt(0)}${this._apePat.charAt(0)}${this._apeMat.charAt(0)}`
+    }
+}
